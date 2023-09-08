00c7c7f627f40a6096094de0c0cd28a9a5afbafb
StephenAnyanwu <voltasteve@gmailcom>
2023-09-08 15:56:41 -0700

    6-size.c coreection

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,18 +4,18 @@
  * main - the main function
  * Return: returns 0 if no error
 */
-
 int main(void)
-{	char a;
+{
+	char a;
 	int b;
 	long int c;
 	long long int d;
 	float e;
-	
+
 	printf("Size of a char: %lu byte(s)\n", sizeof(a));
 	printf("Size of an int: %lu byte(s)\n", sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
-	printf("Size of a float: %lu  byte(s)\n", sizeof(e));
+	printf("Size of a float: %lu byte(s)\n", sizeof(e));
 	return (0);
 }
