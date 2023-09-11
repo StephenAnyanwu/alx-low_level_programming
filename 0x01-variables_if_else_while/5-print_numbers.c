224abd5d6ff05f9f1999d147dc467f92af9db9d4
StephenAnyanwu <voltasteve@gmailcom>
2023-09-10 19:58:11 -0700

    5-print_numbers.c task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - The main function
+ * Return: return 0 if successful
+*/
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+		printf("%d\n", digit);
+	return (0);
+}
