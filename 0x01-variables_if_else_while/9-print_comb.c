f569f187c8eb38025c8b09a4893acca9fddbb5c2
StephenAnyanwu <voltasteve@gmailcom>
2023-09-10 21:46:15 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * Return - returns 0 if successful
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num >= 9; num++)
+	{
+		if (num == 9)
+			putchar(num + '0');
+		else
+		{
+			putchar(num + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
