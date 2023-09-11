3c8cece5282daf9de2e8b710ccd2c473ac69ae23
StephenAnyanwu <voltasteve@gmailcom>
2023-09-10 19:12:45 -0700

    2-print_alphetic task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - The main function
+ *Return: returns 0 if successful
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
+
