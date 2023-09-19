fe8aab0fbb482e9c69fa06edb29d70aab3377d80
StephenAnyanwu <voltasteve@gmailcom>
2023-09-19 09:39:06 -0700

    second commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,33 @@
 #include "main.h"
 
 /**
+ * _strlen - a function that returns the length of a string.
+ * @s: a pointer to the string (parameter).
+ * Return: return an integer 
+*/
+int _strlen(char *s)
+{
+	int str_len = 0;
+
+	while (*s)
+	{
+		str_len++;
+		s++;
+	}
+	return (str_len);
+}
+
+/**
  * _puts - a function that prints a string.
  * @str: a pointer of the string that is to be  printed (parameter).
 */
 void _puts(char *str)
 {
+	int str_len;
 	int i;
-	int str_length;
 
-	str_length = _strlen(str);
-	for (i = 0; i < str_length; i++)
+	str_len = _strlen(str);
+	for (i = 0; i < str_len; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
