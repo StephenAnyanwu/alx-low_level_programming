0e4693bb62993dbc22cec089ec5bb7bdf35a64d6
StephenAnyanwu <voltasteve@gmailcom>
2023-09-19 14:54:24 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _strlen - a function that returns the length of a string.
+ * @str: a string (parameter).
+ * Return: returns an integer (length of s).
+*/
+int _str_length(char *str)
+{
+	int i = 0, count = 0;
+
+	while (str[i] != '\0')
+	{
+		count++;
+		i++;
+	}
+	return (count);
+}
+
+/**
+ * _putchar - a function that prints a character.
+ * @c: a character to be printed (parameter).
+ * Return: return if successful.
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * rev_string - a function that prints a string, in reverse.
+ * @s: a pointer to a string (parameter).
+*/
+void rev_string(char *s)
+{
+	int length = _str_length(s), i, idx;
+
+	for (i = 0; i < length; i++)
+	{
+		idx = length - (i + 1);
+		_putchar(s[idx]);
+	}
+	_putchar('\n');
+}
