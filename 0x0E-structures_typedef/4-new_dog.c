2b01b751d4b0062882948e5523d2c1dd2bb1b2dd
StephenAnyanwu <voltasteve@gmailcom>
2023-10-12 02:24:04 -0700

    task 4 second commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -35,7 +35,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (newDog->name == NULL)
 	{
 		free(newDog);
-		free(newDog->name);
 		return (NULL);
 	}
 	for (i = 0; i < name_len; i++)
@@ -44,7 +43,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (newDog->owner == NULL)
 	{
 		free(newDog);
-		free(newDog->owner);
 		return (NULL);
 	}
 	for (i = 0; i < owner_len; i++)
