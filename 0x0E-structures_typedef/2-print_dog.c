26422fc623633735cdad24bebe1979ae68bff341
StephenAnyanwu <voltasteve@gmailcom>
2023-10-11 23:52:53 -0700

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,14 +11,14 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		if ((*d).name == NULL)
-			printf("Name: (nil)");
-		else if ((*d).age == 0 || (*d).owner == NULL)
-			printf("(nil)");
+			printf("Name: (nil)\n");
 		else
-		{
 			printf("Name: %s\n", (*d).name);
-			printf("Age: %f\n", (*d).age);
+		printf("Age: %f\n", (*d).age);
+		if ((*d).owner == NULL)
+			printf("Owner: (nil)\n");
+		else
 			printf("Owner: %s\n", (*d).owner);
-		}
+		
 	}
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * struct dog - dog structure.
  * @name: first nember
  * @age: second member.
  * @owner: third member
 */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
