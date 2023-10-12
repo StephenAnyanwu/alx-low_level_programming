32ae6619c4d606884bb346b662e4e029e70f44d6
StephenAnyanwu <voltasteve@gmailcom>
2023-10-12 04:00:42 -0700

    task 4 sixth commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -43,6 +43,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	newDog->owner = malloc(sizeof(char) * owner_len);
 	if (newDog->owner == NULL)
 	{
+		free(newDog->name);
 		free(newDog->owner);
 		free(newDog);
 		return (NULL);
