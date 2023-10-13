17f388943572848151f38001a1bbe0426fdae0a0
StephenAnyanwu <voltasteve@gmailcom>
2023-10-13 16:19:28 -0700

    task 0 second commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
  * print_name - a function that prints name.
  * @name: name to be printed (a pointer).
@@ -5,5 +7,7 @@
 */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
 }
