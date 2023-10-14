72db567b727be0a9044bf5959d5f673bad7fa3e1
StephenAnyanwu <voltasteve@gmailcom>
2023-10-13 17:45:19 -0700

    task 2 second commit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,7 +15,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
-	for (i = size; i < size; i++)
+	for (i = 0; i < size; i++)
 		if (cmp(array[i]) == 1)
 			return (i);
 	return (-1);
