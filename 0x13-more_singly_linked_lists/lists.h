f50f4c2d825c51ea6b8fb8227c3e3f2cd77c1218
StephenAnyanwu <voltasteve@gmailcom>
2023-10-23 12:45:07 -0700

    task 0 thir commit

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -8,12 +8,12 @@
  * Description: singly linked list node structure
  *
  */
-#include <stddef.h>
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
+#include <stddef.h>
 size_t print_listint(const listint_t *h);
 
 
