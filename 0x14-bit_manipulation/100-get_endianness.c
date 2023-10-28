74cf82bda24cdecf0409e4e69d0959a0bac9412e
StephenAnyanwu <voltasteve@gmailcom>
2023-10-27 18:42:52 -0700

    task 5 first commit

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * int get_endianness -  a function that checks the endianness.
+ * 
+ * Return: 0 if big endian, 1 if little endian.
+*/
+
+int get_endianness(void)
+{
+	
+}
