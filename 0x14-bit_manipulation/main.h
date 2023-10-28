ca149d7ddfc192fab27bd548703827e56ac29ae8
StephenAnyanwu <voltasteve@gmailcom>
2023-10-27 17:32:28 -0700

    task 2 first commit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,13 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 int _putchar(char c);
-typedef struct binary
-{
-	unsigned long int exp;
-	struct binary *next;
-} b;
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
-void add_node_end(b **head, unsigned long int exp);
+int get_bit(unsigned long int n, unsigned int index);
 
 #endif
