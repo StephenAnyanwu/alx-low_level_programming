98a14ea47aabd54fc9c7b84bdcee3aaef26d83c9
StephenAnyanwu <voltasteve@gmailcom>
2023-12-08 05:53:23 -0800

    task 7 second commit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -43,7 +43,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = NULL;
-	if (list_len == 0)
+	if (list_len == 0 && idx == 0)
 	{
 		*h = new_node;
 		return (new_node);
