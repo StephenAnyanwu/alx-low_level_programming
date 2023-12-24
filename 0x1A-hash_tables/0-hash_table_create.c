54369a7b4b130729a12d25ebacb6a06d13e12bb6
StephenAnyanwu <voltasteve@gmailcom>
2023-12-24 08:25:46 -0800

    task 3 corr

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,13 +10,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
 	/* creates a new hash table */
-	hash_table_t *table = malloc(sizeof(hash_table_t));
+	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
-	/* creates an array */
-	table->array = calloc(table->size, sizeof(hash_node_t *));
+	/* creates an array that stores hash_node_t pointers*/
+	table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t *));
 	if (table->array == NULL)
 		return (NULL);
 	/* initializes the array with NULL */
--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,21 +8,24 @@
 /* >> Compute the index based on the hash function. */
 /* >> Check if the index is already occupied or not, by comparing the key.*/
 /*	>> If it is not occupied, you can directly insert it into index. */
-/*	>> Otherwise, it is a collision, and you will need to handle it. */
+/*	>> Otherwise, it is a collision, and you will need to handle it, by */
+/*	>> adding the new item (node) at the beginning of the linked list */
 
 
 /**
  * create_item - creates an item (a node).
- * @key: key
- * @value: value
+ * @key: key of the item.
+ * @value: value of the item.
  * Return: a pointer to the item created.
 */
 hash_node_t *create_item(const char *key, const char *value)
 {
-	/* a pointer to a new hash_table_t item */
+	/* a pointer to a hash_table_t item */
 	hash_node_t *item = malloc(sizeof(hash_node_t));
 
+	/* allocates memory to the item key */
 	item->key = malloc(strlen(key) + 1);
+	/* allocates memory to the item value */
 	item->value = malloc(strlen(value) + 1);
 	if (item == NULL || item->key == NULL || item->value == NULL)
 		return (NULL);
@@ -35,17 +38,25 @@ hash_node_t *create_item(const char *key, const char *value)
 /**
  * hash_table_set - adds an element to the hash table.
  * @ht: the hash table.
- * @key: key.
- * @value: value.
+ * @key: key of item (can not be an empty string).
+ * @value: value of item (can be an empty string).
  * Return: 1 if it succeeded, 0 otherwise.
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int idx = key_index((const unsigned char *)key, ht->size);
-	hash_node_t *item = create_item(key, value);
-	hash_node_t *item_loc = ht->array[idx];
+	unsigned long int idx;
+	hash_node_t *item, *item_loc;
 
-	/* location to add item in array is empty */
+	if (strlen(key) == 0)
+		return (0);
+	/* finds the index of the key using a hash function */
+	idx = key_index((const unsigned char *)key, ht->size);
+	/* creates an item (a node) */
+	item = create_item(key, value);
+	/* location in the array where to store the created item */
+	item_loc = ht->array[idx];
+
+	/* if location to add item in array is empty */
 	if (item_loc == NULL)
 	{
 		ht->array[idx] = item; /* insert directly */
